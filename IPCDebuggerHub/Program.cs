018a5d6f5687285a0b52ff989c7a7029ce052da3
Hailey <Hexmaster111@gmail.com>
2023-06-09 17:55:07 -0500

    .

--- IPCDebuggerHub/Program.cs
+++ IPCDebuggerHub/Program.cs
@@ -2,16 +2,23 @@
 
 using IPCDebuggerContract;
 using IPCMarshall;
+using IPCMarshall.ValueTypes;
 
 var memManager = new IPCMemServer<MyStruct>(nameof(MyStruct), TimeSpan.FromMilliseconds(50));
 
 Console.WriteLine("Hub Starting");
 
-int i = 0;
 
+
+int i = 0;
+bool toggle = false;
 while (true)
 {
-    Thread.Sleep(500);
+    Thread.Sleep(1000);
+
+
+    toggle = !toggle;
+
     var a = new MyStruct($"Ima string!", i++);
     var written = memManager.Write(ref a);
     Console.WriteLine($"Written: {written} - {a.Counter} - {a.StringOne}");
